1e7c2123b3d92429f8099a14453b0dabe561b658
Arthur <arthurvarteressian@gmail.com>
2024-10-27 17:22:14 +0100

    Fixed components issue

--- frontend/src/AppHeader.vue
+++ frontend/src/AppHeader.vue
@@ -1,8 +1,8 @@
 <!-- src/AppHeader.vue -->
 <template>
   <header class="bg-slate-300 p-2 md:p-4 flex flex-col md:flex-row items-center justify-between">
-    <div class="flex items-center">
-      <img src="./assets/bookStore.jpg" alt="Book Store" class="w-12 h-12 md:w-16 md:h-16 cursor-pointer p-0" />
+    <div class="flex items-center" @click="redirectToHome" style="cursor: pointer;">
+      <img src="./assets/bookStore.jpg" alt="Book Store" class="w-12 h-12 md:w-16 md:h-16 p-0" />
       <h1 class="ml-2 md:ml-4 text-lg md:text-xl font-bold">Book Store</h1>
     </div>
     <p class="hidden md:block text-gray-600 pr-5 text-sm md:text-base">Buch & Wein</p>
@@ -10,4 +10,10 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const redirectToHome = () => {
+  router.replace('/'); // Navigate to the home route without reloading
+};
 </script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import Toast from 'vue-toastification'; // Import Vue Toastification
 import 'vue-toastification/dist/index.css'; // Import Vue Toastification CSS
-// import App from './App.vue';
+import App from './App.vue';
 import router from './router';
-import AppMainPage from './AppMainPage.vue'; // Change this line to AppMainPage
 
-const app = createApp(AppMainPage);
+const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
