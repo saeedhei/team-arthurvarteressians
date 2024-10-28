85f4ab864de8c55c74d872d07d78dec44aa9f07c
Arthur <arthurvarteressian@gmail.com>
2024-10-28 14:57:16 +0100

    Change Main Layout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import Toast from 'vue-toastification'; // Import Vue Toastification
 import 'vue-toastification/dist/index.css'; // Import Vue Toastification CSS
-// import App from './App.vue';
+import App from './App.vue';
 import router from './router';
 import TT from './TT.vue';
-const app = createApp(TT);
+const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
