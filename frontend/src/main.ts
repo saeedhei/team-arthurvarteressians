47fe5bccaa8ddb9ed458b0eed452dec94fe89a00
Arthur <arthurvarteressian@gmail.com>
2024-10-29 21:56:46 +0100

    Added Skeleton animation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,14 @@
-import './assets/main.css';
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Toast from 'vue-toastification' // Import Toastification
+import 'vue-toastification/dist/index.css' // Import Toastification CSS
+import App from './App.vue'
+import router from './router'
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import Toast from 'vue-toastification'; // Import Vue Toastification
-import 'vue-toastification/dist/index.css'; // Import Vue Toastification CSS
-import App from './App.vue';
-import router from './router';
-// import TT from './TT.vue';
-const app = createApp(App);
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(Toast) // Added for Toast
 
-app.use(createPinia());
-app.use(router);
-app.use(Toast); // Register Vue Toastification as a plugin
-
-app.mount('#app');
+app.mount('#app')
