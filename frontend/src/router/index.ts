142cd7e52749fff7e20f75b95197bc2f0a6abe0b
Arthur <arthurvarteressian@gmail.com>
2024-10-31 13:29:54 +0100

    Cleaned up Frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // Had an issue, changed tsconfig.json file
   routes,
 });
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -11,6 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    
   ],
   resolve: {
     alias: {
